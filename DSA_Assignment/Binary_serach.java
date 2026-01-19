8c5d91b9ae1f759d383525b71cb7b1af939364b6
Kaushal041 <kaushalrawatja8@gmail.com>
2026-01-19 22:30:01 +0530

    Fix target value in Binary Search implementation

--- DSA_Assignment/Binary_serach.java
+++ DSA_Assignment/Binary_serach.java
@@ -3,7 +3,7 @@ public class Binary_serach {
         
         //----------------- Binary Search ----------------//
         int [] arr = {10,20,30,40,50,60,70,80,90,100};
-        int target = 100;
+        int target = 10;
         int start = 0;
         int end = arr.length-1;
         int mid = (start + end)/2;
@@ -19,7 +19,7 @@ public class Binary_serach {
                 end = mid - 1;
                 mid = (start + end)/2;
             }
-            else if(target > arr[mid]){
+            else{
                 start = mid + 1;
                 mid = (start + end)/2;
             }
